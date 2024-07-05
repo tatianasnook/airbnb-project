fc58ebda62a6c80fae37ed694b5d45dc7b2a9a12
tatianasnook <tatsnook@gmail.com>
2024-07-05 15:41:19 -0500

    added Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import logo from '../images/airbnb-logo.png';
+
+const Navbar = () => {
+  return (
+    <nav className='nav'>
+      <img src={ logo } alt="Airbnb logo" />
+    </nav>
+  )
+}
+
+export default Navbar;
